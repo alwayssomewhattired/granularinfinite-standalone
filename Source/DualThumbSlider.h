6ce9af1e33e49cca07472797a0b1e1b4cd366b8c
Zachary Malinka <zacharymalinka@gmail.com>
2025-12-05 08:31:47 -0500

    *COMPLETED* - made rnage slider have a mid thumb
    *TO-DO*
    - make grain area slider use range slider

--- Source/DualThumbSlider.h
+++ Source/DualThumbSlider.h
@@ -1,76 +1,121 @@
-#include <juce_gui_basics/juce_gui_basics.h>
 #pragma once
+#include <juce_gui_basics/juce_gui_basics.h>
 
 class DualThumbSlider : public juce::Slider
 {
 public:
-	DualThumbSlider(std::tuple<double, double, double> range)
-	{
-		double min = std::get<0>(range);
-		double max = std::get<1>(range);
-		double interval = std::get<2>(range);
+    DualThumbSlider(std::tuple<double, double, double> range)
+    {
+        setRange(std::get<0>(range), std::get<1>(range), std::get<2>(range));
+        minValue = std::get<0>(range);
+        maxValue = std::get<1>(range);
+    }
 
-		setRange(min, max, interval);
+    enum DragType { DragNone, DragMin, DragMax, DragMid };
 
-		minValue = min;
-		maxValue = max;
+    void paint(juce::Graphics& g) override
+    {
+        auto bounds = getLocalBounds().toFloat();
+        auto minPos = valueToProportionOfLength(minValue);
+        auto maxPos = valueToProportionOfLength(maxValue);
 
-	}
+        float minX = bounds.getX() + bounds.getWidth() * minPos;
+        float maxX = bounds.getX() + bounds.getWidth() * maxPos;
+        float midX = (minX + maxX) * 0.5f;
 
-	void paint(juce::Graphics& g) override
-	{
-		auto bounds = getLocalBounds().toFloat();
-		auto minPos = valueToProportionOfLength(minValue);
-		auto maxPos = valueToProportionOfLength(maxValue);
+        // Track
+        g.setColour(juce::Colours::darkgrey);
+        g.fillRect(bounds.reduced(0, bounds.getHeight() / 2.25f));
 
-		auto minX = bounds.getX() + bounds.getWidth() * minPos;
-		auto maxX = bounds.getX() + bounds.getWidth() * maxPos;
+        // Highlight range
+        g.setColour(juce::Colours::green);
+        g.fillRect(juce::Rectangle<float>(minX, bounds.getCentreY() - 4, maxX - minX, 8));
 
-		// Track
-		g.setColour(juce::Colours::darkgrey);
-		g.fillRect(bounds.reduced(0, bounds.getHeight() / 2.25));
+        // Draw thumbs
+        g.setColour(juce::Colours::white);
+        drawThumb(g, minX);
+        drawThumb(g, maxX);
+        drawThumb(g, midX);
+    }
 
-		// Range highlight
-		g.setColour(juce::Colours::green);
-		g.fillRect(juce::Rectangle<float>(minX, bounds.getCentreY() - 4, maxX - minX, 8));
+    void mouseDown(const juce::MouseEvent& e) override
+    {
+        auto bounds = getLocalBounds().toFloat();
+        float x = (float)e.position.x;
 
-		// Thumbs
-		g.setColour(juce::Colours::white);
-		g.fillEllipse(minX, bounds.getCentreY() - 6, 12, 12);
-		g.fillEllipse(maxX - 12, bounds.getCentreY() - 6, 12, 12);
-	}
+        float minX = bounds.getWidth() * valueToProportionOfLength(minValue);
+        float maxX = bounds.getWidth() * valueToProportionOfLength(maxValue);
+        float midX = (minX + maxX) * 0.5f;
 
-	void mouseDown(const juce::MouseEvent& e) override
-	{
-		draggingMin = std::abs(proportionOfLengthToValue(e.position.x / getWidth()) - minValue)
-			< std::abs(proportionOfLengthToValue(e.position.x / getWidth()) - maxValue);
-	}
+        float distMin = std::abs(x - minX);
+        float distMax = std::abs(x - maxX);
+        float distMid = std::abs(x - midX);
 
-	std::function<void()> onRangeChange;
+        float nearest = std::min({ distMin, distMax, distMid });
 
-	// works
-	void mouseDrag(const juce::MouseEvent& e) override
-	{
+        if (nearest == distMin) dragging = DragMin;
+        else if (nearest == distMax) dragging = DragMax;
+        else dragging = DragMid;
 
+        originalMin = minValue;
+        originalMax = maxValue;
+    }
 
-		double newValue = proportionOfLengthToValue(e.position.x / getWidth());
-		if (draggingMin)
-			minValue = juce::jlimit(getMinimum(), maxValue, newValue);
-		else
-			maxValue = juce::jlimit(minValue, getMaximum(), newValue);
+    void mouseDrag(const juce::MouseEvent& e) override
+    {
+        double newVal = proportionOfLengthToValue(e.position.x / getWidth());
 
-		repaint();
+        switch (dragging)
+        {
+        case DragMin:
+            minValue = juce::jlimit(getMinimum(), maxValue, newVal);
+            break;
 
-		if (onRangeChange) {
-			onRangeChange();
-		}
-	}
+        case DragMax:
+            maxValue = juce::jlimit(minValue, getMaximum(), newVal);
+            break;
 
-	double getMinValue() const { return minValue; }
-	double getMaxValue() const { return maxValue; }
+        case DragMid:
+        {
+            double range = originalMax - originalMin;
+            double center = newVal;
+
+            double newMin = center - range * 0.5;
+            double newMax = center + range * 0.5;
+
+            // Clamp without changing width
+            double offset = 0.0;
+
+            if (newMin < getMinimum())
+                offset = getMinimum() - newMin;
+            else if (newMax > getMaximum())
+                offset = getMaximum() - newMax;
+
+            minValue = newMin + offset;
+            maxValue = newMax + offset;
+            break;
+        }
+        }
+
+        repaint();
+        if (onRangeChange) onRangeChange();
+    }
+
+    double getMinValue() const { return minValue; }
+    double getMaxValue() const { return maxValue; }
+
+    std::function<void()> onRangeChange;
 
 private:
-	double minValue = 25.0;
-	double maxValue = 75.0;
-	bool draggingMin = true;
+    void drawThumb(juce::Graphics& g, float x)
+    {
+        g.fillEllipse(x - 6.0f, getHeight() / 2.0f - 6.0f, 12.0f, 12.0f);
+    }
+
+    double minValue = 25.0;
+    double maxValue = 75.0;
+    double originalMin = 25.0;
+    double originalMax = 75.0;
+
+    DragType dragging = DragNone;
 };
